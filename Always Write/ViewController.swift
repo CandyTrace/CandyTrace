2d126cbd79e94fb4bd4ac4f85808f7900ea0303e
Sam Raudabaugh <sam@Sams-MacBook-Pro.local>
2017-03-10 20:06:21 -0500

    Initial Commit

---
+++ Always Write/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Always Write
+//
+//  Created by Sam Raudabaugh on 3/10/17.
+//  Copyright © 2017 Cornell Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
